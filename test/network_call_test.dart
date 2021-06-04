6a40a9e06b5bc11d22836933a0089cfb01406b64
Amjad Khan <amjadalam2k16@gmail.com>
2021-06-04 14:13:00 +0500

    api key removed

--- test/network_call_test.dart
+++ test/network_call_test.dart
@@ -2,19 +2,17 @@ import 'package:demo/helpers/http_service.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 main() {
+  String _api = '';
   test('return no city found error', () async {
-    final result = await HttpService.getRequest(
-        apiKey: '5cec6ddcf992b47d01c3c33559e54a2e', city: 'Londo');
+    final result = await HttpService.getRequest(apiKey: _api, city: 'Londo');
     expect(result['message'], 'city not found');
   });
   test('return London Weather', () async {
-    final result = await HttpService.getRequest(
-        apiKey: '5cec6ddcf992b47d01c3c33559e54a2e', city: 'London');
+    final result = await HttpService.getRequest(apiKey: _api, city: 'London');
     expect(result['name'], 'London');
   });
   test('wrong api key error ', () async {
-    final result = await HttpService.getRequest(
-        apiKey: '5cec6ddcf992b47d01c3c33559e54a2es', city: 'London');
+    final result = await HttpService.getRequest(apiKey: _api, city: 'London');
     expect(result['message'],
         'Invalid API key. Please see http://openweathermap.org/faq#error401 for more info.');
   });
